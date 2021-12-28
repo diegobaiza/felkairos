463da170d4b67ea4970565ca5cab426211643813
Valerin Saban <valerinsaban24@gmail.com>
2021-12-28 01:29:09 -0600

    Fix Token

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,8 +1,13 @@
 import Sequelize from 'sequelize';
-import token from '../middleware/token.js';
+import jwt from 'jsonwebtoken';
+import key from '../middleware/key.js';
 
 function db(req) {
-  return new Sequelize(token(req.headers.authorization), 'root', '', {
+  let token = req;
+  if (typeof req == 'object') {
+    token = (jwt.verify(req.headers.authorization, key)).database;
+  }
+  return new Sequelize(token, 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-06:00',
--- backend/src/middleware/token.js
+++ backend/src/middleware/token.js
@@ -2,8 +2,13 @@ import jwt from 'jsonwebtoken';
 import key from './key.js';
 
 function token(tk) {
+  console.log(key);
+  try {
     let token = jwt.verify(tk, key);
     return token.database;
+  } catch (error) {
+    return tk;
+  }
 }
 
 export default token;
